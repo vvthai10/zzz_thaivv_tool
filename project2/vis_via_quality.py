ed056cc04fe1e68fbff2e6eacf53da5a0c3f1fed
vvthai <vuvanthai1410@gmail.com>
2025-07-10 23:25:06 +0700

    update

--- project2/vis_via_quality.py
+++ project2/vis_via_quality.py
@@ -265,6 +265,87 @@ def overlay(
     
     return image_combined
 
+from shapely.geometry import Polygon
+from collections import defaultdict, deque
+
+def sort_segments_by_overlap(data, threshold=0.05):
+    """
+    data: list các dict chứa keys: 'region_attributes' + 'shape_attributes'
+    Return:
+        - sorted_data: data đã reorder theo area nhỏ trước area lớn cho các cặp overlap
+        - overlap_pairs: list tuple (idx_small, label_small, area_small, idx_large, label_large, area_large, iou)
+    """
+    polygons = []
+    
+    for idx, seg in enumerate(data):
+        if seg is None:
+            continue
+        try:
+            label = seg['region_attributes']['name']
+            x = seg['shape_attributes']['all_points_x']
+            y = seg['shape_attributes']['all_points_y']
+            coords = list(zip(x, y))
+            poly = Polygon(coords)
+            if not poly.is_valid:
+                poly = poly.buffer(0)
+            area = poly.area
+            polygons.append( (idx, label, poly, area, seg) )
+        except:
+            continue
+    
+    # Build graph & overlap_pairs
+    graph = defaultdict(set)
+    indegree = defaultdict(int)
+    overlap_pairs = []
+
+    for i in range(len(polygons)):
+        for j in range(i+1, len(polygons)):
+            idx1, label1, poly1, area1, seg1 = polygons[i]
+            idx2, label2, poly2, area2, seg2 = polygons[j]
+            if not poly1.is_valid or not poly2.is_valid:
+                continue
+            inter_area = poly1.intersection(poly2).area
+            union_area = poly1.union(poly2).area
+            if union_area == 0:
+                continue
+            iou = inter_area / union_area
+            if iou > threshold:
+                if area1 <= area2:
+                    graph[idx1].add(idx2)
+                    indegree[idx2] += 1
+                    overlap_pairs.append( (idx1, label1, area1, idx2, label2, area2, iou) )
+                else:
+                    graph[idx2].add(idx1)
+                    indegree[idx1] += 1
+                    overlap_pairs.append( (idx2, label2, area2, idx1, label1, area1, iou) )
+    
+    # Topological sort
+    queue = deque()
+    for idx, _, _, _, _ in polygons:
+        if indegree[idx] == 0:
+            queue.append(idx)
+    
+    sorted_idx = []
+    while queue:
+        u = queue.popleft()
+        sorted_idx.append(u)
+        for v in graph[u]:
+            indegree[v] -= 1
+            if indegree[v] == 0:
+                queue.append(v)
+
+    # Append idx not in sorted_idx yet
+    all_idx = [idx for idx, _, _, _, _ in polygons]
+    for idx in all_idx:
+        if idx not in sorted_idx:
+            sorted_idx.append(idx)
+    
+    # Build final sorted data list
+    idx_to_seg = {idx: seg for idx, _, _, _, seg in polygons}
+    sorted_data = [idx_to_seg[idx] for idx in sorted_idx]
+
+    return sorted_data, overlap_pairs
+
 def overlayColor(
     image: np.ndarray,
     colored_mask: np.ndarray,
@@ -315,6 +396,10 @@ for folder in folderNames:
     if os.path.isdir(dpath+"/results1"):
         shutil.rmtree(dpath +"/results1/")
     os.makedirs(dpath +"/results1")
+    
+    if os.path.isdir(dpath+"/results2"):
+        shutil.rmtree(dpath +"/results2/")
+    os.makedirs(dpath +"/results2")
             
     os.chdir(dpath)
     imgNames = glob.glob("*", recursive = True)
@@ -327,6 +412,8 @@ for folder in folderNames:
     missed_labels = []
     map_missed_labels = {}
     total_segments = []
+    total_segments_2 = []
+    note_overlaps = []
     for _,data_i in enumerate(tqdm(data)):
         img_name = data_i[0]
         if "’" in img_name:
@@ -359,7 +446,9 @@ for folder in folderNames:
         all_contours = []
         found_missing_labels = False
         count_segment = 0
-        for seg in data_i[1]:
+        if data_i[1] == None:
+            continue
+        for seg in data_i[1][::-1]:
             if seg == None:
                 continue
             label = seg['region_attributes']['name']
@@ -479,7 +568,7 @@ for folder in folderNames:
             contour = np.expand_dims(contour, axis=1)
             all_contours.append(contour)
         overlayImg = overlayColor(img, seg_img)
-        
+              
         total_segments.append(
             {
             "image": img_name[:-4],
@@ -527,6 +616,194 @@ for folder in folderNames:
         fn = dpath +"/results1/" + img_name[:dot]+".jpg"
         im_v = cv2.hconcat([img, overlayImg, third_img])
         cv2.imwrite(fn,im_v)
+        
+        # TODO: Handle overlap
+        sorted_data, overlap_pairs = sort_segments_by_overlap(data_i[1])
+        if len(overlap_pairs) > 0:
+            note_overlaps.append(
+                {
+                    "image": img_name[:-4],
+                    "overlaps": overlap_pairs 
+                }
+            )
+            
+            seg_img = np.zeros((img.shape[0], img.shape[1],3), np.uint8)
+            third_img = np.ones((img.shape[0], img.shape[1], 3), dtype=np.uint8) * 255
+            
+            all_centers = []
+            all_contours = []
+            found_missing_labels = False
+            count_segment = 0
+            if data_i[1] == None:
+                continue
+            for seg in sorted_data:
+                if seg == None:
+                    continue
+                label = seg['region_attributes']['name']
+                try:
+                    x = seg['shape_attributes']['all_points_x']
+                except:
+                    continue
+                x = seg['shape_attributes']['all_points_x']
+                y = seg['shape_attributes']['all_points_y']
+                
+                count_segment += 1
+                if fuzz.partial_ratio('background', label) > 95: #cho dress
+                    # if label != "background":
+                    #     print(f"{label} with {fuzz.partial_ratio('background', label)}")
+                    color = c14
+                elif fuzz.partial_ratio('hat', label) > 95: #cho dress
+                    color = c15
+                elif fuzz.partial_ratio('hair', label) > 95:
+                    color = c1
+                elif fuzz.partial_ratio('watch', label) > 95:
+                    color = c2
+                elif fuzz.partial_ratio('glasses', label) > 95\
+                    or fuzz.partial_ratio('mask', label) > 95:
+                    color = c3
+                elif fuzz.partial_ratio('left hand skin', label) > 95:
+                    color = c4
+                elif fuzz.partial_ratio('right hand skin', label) > 95:
+                    color = c8
+                elif fuzz.partial_ratio('left leg skin', label) > 95:
+                    color = c10
+                elif fuzz.partial_ratio('right leg skin', label) > 95:
+                    color = c18
+                elif fuzz.partial_ratio('body skin', label) > 95:
+                    color = c19
+                elif fuzz.partial_ratio('face skin', label) > 95:
+                    color = c5
+                elif fuzz.partial_ratio('scarf', label) > 95:
+                    color = c5
+                elif fuzz.partial_ratio('person bag', label) > 95:
+                    color = c6
+                elif fuzz.partial_ratio('top', label) > 95:
+                    color = c7
+                elif fuzz.partial_ratio('dress', label) > 95:
+                    color = c11
+                elif fuzz.partial_ratio('skirt', label) > 95:
+                    color = c12
+                elif fuzz.partial_ratio('ring', label) > 95:
+                    color = c13
+                elif fuzz.partial_ratio('left pants', label) > 95 :
+                    color = c14
+                elif fuzz.partial_ratio('right pants', label) > 95:
+                    color = c15
+                elif fuzz.partial_ratio('left sleeves', label) > 95:
+                    color = c16
+                elif fuzz.partial_ratio('right sleeves', label) > 95:
+                    color = c17
+                elif fuzz.partial_ratio('left shoes', label) > 95 or fuzz.partial_ratio('boots', label) > 95:
+                    # if label != "left shoes":
+                        # print(f"{label} with {fuzz.partial_ratio('left shoes', label)}")
+                    color = c18
+                elif fuzz.partial_ratio('right shoes', label) > 95 or\
+                    fuzz.partial_ratio('boots', label) > 95:
+                    # if label != "right shoes":
+                    #     print(f"{label} with {fuzz.partial_ratio('right shoes', label)}")
+                    color = c19
+                elif fuzz.partial_ratio('left tights', label) > 95:
+                    color = c20
+                elif fuzz.partial_ratio('right tights', label) > 95:
+                    color = c21
+                elif fuzz.partial_ratio('left socks', label) > 95:
+                    color = c22
+                elif fuzz.partial_ratio('right socks', label) > 95:
+                    color = c23
+                elif fuzz.partial_ratio('band', label) > 95:
+                    color = c24
+                elif fuzz.partial_ratio('gloves', label) > 95:
+                    color = c25
+                elif fuzz.partial_ratio('jumpsuit', label) > 95:
+                    color = c26
+                elif fuzz.partial_ratio('inner top', label) > 95:
+                    color = c27
+                elif fuzz.partial_ratio('inner bottom', label) > 95:
+                    color = c32
+                elif fuzz.partial_ratio('inner dress', label) > 95:
+                    color = c33
+                elif fuzz.partial_ratio('inner sleeve', label) > 95:
+                    color = c34
+                elif fuzz.partial_ratio('panty', label) > 95:
+                    color = c28
+                elif fuzz.partial_ratio('hair accessory', label) > 95:
+                    color = c29
+                elif fuzz.partial_ratio('1-piece swimwear', label) > 95:
+                    color = c30
+                elif fuzz.partial_ratio('whole body long', label) > 95:
+                    color = c31
+                else:
+                    count_segment -= 1
+                    # print(img_name)
+                    # print(label)
+                    color = c9
+                    found_missing_labels = True
+                    # missed_labels.append(img_name)
+                    # if img_name not in map_missed_labels.keys():
+                    #     map_missed_labels[img_name] = []
+                    # map_missed_labels[img_name].append(label)
+
+                color2 = ( int (color [ 0 ]), int (color [ 1 ]), int (color [ 2 ])) 
+                arr = np.array([x,y])
+                # print("CHECK HERE")
+                # print(arr)
+                center = arr.mean(1)
+                all_centers.append([label, center])
+                cv2.fillPoly(seg_img, pts = [np.transpose(arr)], color=color2)
+                cv2.fillPoly(third_img, pts = [np.transpose(arr)], color=color2)
+            
+                contour = np.transpose(arr)
+                contour = np.expand_dims(contour, axis=1)
+                all_contours.append(contour)
+            overlayImg = overlayColor(img, seg_img)
+                
+            total_segments_2.append(
+                {
+                "image": img_name[:-4],
+                "total": count_segment 
+                }
+            )
+            # if total_segments[0]["image"][0].isdigit():
+            #     total_segments.sort(key=lambda x: int(x["image"].split("_")[0]), reverse=False)
+            # else:
+            #     total_segments.sort(key=lambda x: get_id_from_images_name(x["image"]), reverse=False)
+
+            # print(total_segments)
+            
+            f = 'total label: {}'.format(len(data_i[1]))
+            # if len(data_i[1]) == 0:
+            #     missed_labels.append(img_name)
+            #     if img_name not in map_missed_labels.keys():
+            #         map_missed_labels[img_name] = []
+            #     # print(img_name)
+            #     found_missing_labels = True
+
+            fontsize = max(0.5,overlayImg.shape[0]/1024*0.5)
+            overlayImg = cv2.putText(overlayImg, f, org, font, fontsize, (255, 0, 0), 1, cv2.LINE_AA)  
+            third_img = cv2.putText(third_img, f, org, font, fontsize, (255, 0, 0), 1, cv2.LINE_AA)  
+            
+            for name_center in all_centers:
+                name = name_center[0].strip()
+                center = name_center[1]
+                fontsize = max(0.5,overlayImg.shape[0]/1024*0.5)
+                cv2.circle(overlayImg, (int(center[0]), int(center[1])), 2,  (0, 0, 255), thickness=2)
+                cv2.circle(third_img, (int(center[0]), int(center[1])), 2,  (0, 0, 255), thickness=2)
+                overlayImg = cv2.putText(overlayImg, name, (int(center[0]), int(center[1])), font,fontsize, (0, 255, 0), 1, cv2.LINE_AA) 
+                third_img = cv2.putText(third_img, name, (int(center[0]), int(center[1])), font,fontsize, (0, 255, 0), 1, cv2.LINE_AA) 
+            
+            for contour in all_contours:
+                col = list(np.random.choice(range(256), size=3))
+                cv2.drawContours(overlayImg, contour, -1, ( int (col[ 0 ]), int (col [ 1 ]), int (col [ 2 ])), 4, cv2.FILLED)
+                cv2.drawContours(third_img, contour, -1, ( int (col[ 0 ]), int (col [ 1 ]), int (col [ 2 ])), 4, cv2.FILLED)
+                
+            dot = img_name.find(".")
+            # if found_missing_labels:
+            #     os.makedirs(dpath +"/results1/missing", exist_ok=True)
+            #     fn = dpath +"/results1/missing/" + img_name[:dot]+".jpg"
+            # else:
+            fn = dpath +"/results2/" + img_name[:dot]+".jpg"
+            im_v = cv2.hconcat([img, overlayImg, third_img])
+            cv2.imwrite(fn,im_v)
     
 
     # with open("sample.json", "w", encoding="utf-8") as file:
@@ -546,4 +823,14 @@ for folder in folderNames:
                 fp.write("%s\n" % item)
                 for value in values_missed:
                     fp.write(f"\t{value}\n")
+    
+    if len(note_overlaps) > 0:
+        fn = dpath +'/results1/overlaps.txt'      
+        with open(fn, 'w', encoding='utf-8') as fp:
+            for item in note_overlaps:
+                name = item["image"]
+                fp.write("%s\n" % name)
+                for value in item["overlaps"]:
+                    _, label_1, _, _, label_2, _, _ = value
+                    fp.write(f"\t{label_1} - {label_2}\n")
     print('Done\n')
