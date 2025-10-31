7b1c8b5221f65101b72d49f7dfd11a688ea232e9
vvthai <vuvanthai1410@gmail.com>
2025-11-01 00:10:29 +0700

    update

--- adhoc/main.py
+++ adhoc/main.py
@@ -1,40 +1,43 @@
 import pandas as pd
 import glob
-import os   
+import os  
+from rapidfuzz import fuzz 
 
-SAVE_PATH = "D:\\fix_viettech\\Dataset"
+SAVE_PATH = "/content/drive/MyDrive/viettechtools/Dataset"
 
 def process_folder(folder, image):
     print(f"Processing folder: {folder} with image: {image}")
-    path_phase = "D:\\fix_viettech\\Phase 1"
-    matches = glob.glob(f"{path_phase}/**/{folder}", recursive=True)
-    if len(matches) != 1:
-        print(f"Some mistake in matches folder: {folder}")
-        print(matches)
-        return
-    
-    target_folder = matches[0]
-    image_id = int(image.split("_")[-1])
-    image_files = glob.glob(f"{target_folder}/{image_id}_*", recursive=True)
-    
-    if not os.path.exists(f"{SAVE_PATH}/{image}"):
-        os.makedirs(f"{SAVE_PATH}/{image}")
-        
-    for img_file in image_files:
-        base_name = os.path.basename(img_file)
-        save_file = f"{SAVE_PATH}/{image}/{base_name}"
-        os.system(f"copy \"{img_file}\" \"{save_file}\"")
+    path_phase = "/content/drive/MyDrive/viettechtools/Phase1"
+    list_maker_folders = os.listdir(path_phase)
+    for maker in list_maker_folders:
+      folder_path = os.path.join(path_phase, maker)
+      child_list_folders = [f for f in glob.glob(f"{folder_path}/**", recursive=True) if os.path.isdir(f)]
+      for child_folder in child_list_folders:
+        base_name = os.path.basename(child_folder)
+        if fuzz.partial_ratio(base_name.lower(), folder.lower()) >= 95:
+          target_folder = child_folder
+          image_id = int(image.split("_")[-1])
+          image_files = glob.glob(f"{target_folder}/{image_id}_*", recursive=True)
+
+          if len(image_files) > 0:
+            if not os.path.exists(f"{SAVE_PATH}/{image}"):
+                os.makedirs(f"{SAVE_PATH}/{image}")
+                
+            for img_file in image_files:
+                base_name = os.path.basename(img_file)
+                save_file = f"{SAVE_PATH}/{image}/{base_name}"
+                os.system(f"cp \"{img_file}\" \"{save_file}\"")
     
 
 
 if __name__ == "__main__":
     
-    df = pd.read_csv("./map_phase.csv", header=0)
+    df = pd.read_csv("/content/zzz_thaivv_tool/adhoc//map_phase.csv", header=0)
     map_phase = dict(zip(df["Spree phases"], df["VTT job number"]))
     
     folder_to_check = []
     
-    with open("./original_garment_folders.txt", "r", encoding="utf-8") as f:
+    with open("/content/zzz_thaivv_tool/adhoc/original_garment_folders.txt", "r", encoding="utf-8") as f:
         for line in f:
             line = line.strip()
             phase, folder, image = line.split("\t")
@@ -44,7 +47,7 @@ if __name__ == "__main__":
             if vtt_phase == "-1":
                 continue
             
-            if vtt_phase == 1 and "womens_bottom_page" in folder:
+            if vtt_phase == 1: # and "womens_bottom_page_1" in folder
                 process_folder("_".join(folder.split("_")[:-1]), image)
                 exit()
             
